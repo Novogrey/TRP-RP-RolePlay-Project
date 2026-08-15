f5578f0dc5f99eeb784a045a4ae58b8fc2ef2ce0
Novogrey <olexandrplay228666@gmail.com>
2026-08-15 13:58:18 +0200

    OAuth Fix 3

--- scripts/employee-identifier.js
+++ scripts/employee-identifier.js
@@ -5,7 +5,7 @@
   const API_URL = String(
     window.TRP_APPLICATIONS_API_URL
     || window.TRP_WORKER_IDENTIFIER_API_URL
-    || 'https://script.google.com/macros/s/AKfycbx6VZxf9DEQxZTjYrNPZ9nUio_wfTRECe7CExkOfHdnmHLruJul8658-wg72kQFzIWZ/exec'
+    || 'https://script.google.com/macros/s/AKfycbzIQ4Fck05mlYjPf1QlAj0Lav7szr7ytuSowYeajbnFunFaW40GIfmaS-S91n0vdAhO/exec'
   ).trim();
 
   const COPY = {
--- scripts/training-applications-config.js
+++ scripts/training-applications-config.js
@@ -1,7 +1,7 @@
 window.TRP_APPLICATIONS_API_URL =
   window.TRP_APPLICATIONS_API_URL ||
   window.TRP_TRAINING_APPLICATIONS_API_URL ||
-  "https://script.google.com/macros/s/AKfycbx6VZxf9DEQxZTjYrNPZ9nUio_wfTRECe7CExkOfHdnmHLruJul8658-wg72kQFzIWZ/exec";
+  "https://script.google.com/macros/s/AKfycbzIQ4Fck05mlYjPf1QlAj0Lav7szr7ytuSowYeajbnFunFaW40GIfmaS-S91n0vdAhO/exec";
 
 window.TRP_TRAINING_APPLICATIONS_API_URL =
   window.TRP_TRAINING_APPLICATIONS_API_URL ||
